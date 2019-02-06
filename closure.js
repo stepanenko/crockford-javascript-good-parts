aa1fa5fcddface69ecea3408df1949409d3c1a7d
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-02-06 12:41:09 +0200

    closure

--- closure.js
+++ closure.js
@@ -1,13 +1,12 @@
 
-function outer() {
+var foo = (function outer() {
   var a = 'outer';
 
   return function inner() {
     a = a + 5;
     console.log(a);
   }
-}
-
-var foo = outer();
+})();
 
 foo(); // outer5
+console.log(foo); // [Function: inner]
