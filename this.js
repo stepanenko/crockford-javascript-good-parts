d5fc8e2432d5f4c49033aef7ca2cc1ae6b34f08d
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-02-08 23:23:03 +0200

    this

--- this.js
+++ this.js
@@ -2,9 +2,9 @@
 var obj = {
   name: 'Jack',
   sayHi: function() {
-    console.log(this); // { name: 'Jack', [ Function] }
+    console.log(this); // { name: 'Jack', sayHi: [ Function: sayHi ] }
     return function inner() {
-      console.log(this); // Global Object
+      console.log(this); // Object [global] { ... }
     }
   }
 }
