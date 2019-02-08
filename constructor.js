88d0024da80fc865cf628713fa6762dd2f6ba4d6
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-02-09 00:00:34 +0200

    functions

---
+++ constructor.js
@@ -0,0 +1,8 @@
+
+function Person(name) {
+  this.name = name;
+}
+
+var john = new Person('John');
+
+console.log(john); // Person { name: 'John' }
--- this.js
+++ this.js
@@ -2,9 +2,11 @@
 var obj = {
   name: 'Jack',
   sayHi: function() {
+    var that = this;
     console.log(this); // { name: 'Jack', sayHi: [ Function: sayHi ] }
     return function inner() {
       console.log(this); // Object [global] { ... }
+      console.log(that); // { name: 'Jack', sayHi: [ Function: sayHi ] }
     }
   }
 }
