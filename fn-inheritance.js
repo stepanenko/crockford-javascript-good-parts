ba91de8677d5cc8496eb5661fc082f2a669a52fb
Serhii Stepanenko <sstepane@macmini5478.ddns.softservecom.com>
2019-02-21 17:42:15 +0200

    inheritance

---
+++ fn-inheritance.js
@@ -0,0 +1,26 @@
+
+// ...look at module.js for intro...
+
+// ===  Functional Inheritance  ===
+
+function gizmo(id) {
+  return {
+    // id: id,  // by deleting this line we set an id to be private
+    toString: function () {
+      // return 'gizmo ' + this.id;  // in case we dont want an id to be private
+      return 'gizmo ' + id;
+    }
+  };
+}
+
+function hoozit(id) {
+  var that = gizmo(id);
+
+  that.test = function (testid) {
+    return testid === this.id;
+  };
+
+  return that;
+}
+
+console.log(hoozit(5).toString());  // gizmo 5
--- module.js
+++ module.js
@@ -41,3 +41,35 @@ singleton.secondMethod();  // private place
 }());
 
 methodical.secondMethod(); // hello world
+
+
+// Module pattern is easily transformed into a powerful constructor patterns.
+// ====  Power Constructors:  ====
+// 1. Make an object
+// - Object literal
+// - new
+// - Object.create
+// - call another power constructor
+// 2. Define some private variables and functions
+// 3. Define public methods that closes over the private stuff
+// 4. Return the object
+
+function constructor(spec) {
+  // ===  1  ===
+  var that = {};
+  // ===  2  ===
+  var member = 1;
+  var method = function() {
+    // spec, member, method
+    console.log(member + spec);
+  };
+  // ===  3  ===
+  that.method = method;
+  // ===  4  ===
+  return that;
+}
+
+var myObj = constructor(5);
+var myObj2 = new constructor(5);
+myObj.method();  // 6
+myObj2.method();  // 6
