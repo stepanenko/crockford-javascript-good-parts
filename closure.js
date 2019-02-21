9e90384767ad33de4710cc0476baa77f0ce833fd
Serhii Stepanenko <sstepane@macmini5478.ddns.softservecom.com>
2019-02-21 15:34:49 +0200

    examples on closure

--- closure.js
+++ closure.js
@@ -8,5 +8,26 @@ var foo = (function outer() {
   }
 })();
 
-foo(); // outer5
-console.log(foo); // [Function: inner]
+// foo(); // outer5
+// console.log(foo); // [Function: inner]
+
+
+// ====  MORE EXAMPLES  =====
+
+// SLOW
+var digit_name_slow = function (n) {
+  var names = ['zero', 'one', 'two', 'three'];
+
+  return names[n];
+};
+
+// CLOSURE
+var digit_name = (function (n) {
+  var names = ['zero', 'one', 'two', 'three'];
+
+  return function (n) {
+    return names[n];
+  }
+}());
+
+console.log(digit_name(3)); // three
